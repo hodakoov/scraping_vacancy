f5f0c6914b8c8fa0c737f5f92f501b8214369f88
Valery <teeror93@gmail.com>
2022-11-18 00:12:24 +0400

    1)Добавлено новое приложение - Аккаунты 2)Переопределен стандарный user на кастомный. Теперь для входа нужен емаил, а не логин. 3) Добавлена форма forms.py для отображения на html странице. 4) Изменен html в соответствии с п.3 5) В scraping/views.py добавлено все что связано с формами.

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+
+from scraping.forms import FindForm
 from scraping.models import Vacancy
 
 
 def home(request):
-    print(request.GET)
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', context={'objects_list': qs})
+    # print(request.GET)
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter["city__slug"] = city
+        if language:
+            _filter["language__slug"] = language
+
+        qs = Vacancy.objects.filter(**_filter)
+
+    return render(request, 'scraping/home.html', context={'objects_list': qs,
+                                                          'form': form})
